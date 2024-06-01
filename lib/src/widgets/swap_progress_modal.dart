3450f6ee45b647a8c080c73c01b42debb39f602d
Konstantin Ullrich <konstantinullrich12@gmail.com>
2024-06-01 23:31:22 +0200

    Show Errors in Bridge Sheet

--- lib/src/widgets/swap_progress_modal.dart
+++ lib/src/widgets/swap_progress_modal.dart
@@ -3,9 +3,12 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:frankencoin_wallet/generated/i18n.dart';
 import 'package:frankencoin_wallet/src/colors.dart';
+import 'package:frankencoin_wallet/src/core/bottom_sheet_service.dart';
+import 'package:frankencoin_wallet/src/di.dart';
 import 'package:frankencoin_wallet/src/entities/blockchain.dart';
 import 'package:frankencoin_wallet/src/widgets/primary_fullwidth_button.dart';
 import 'package:frankencoin_wallet/src/widgets/step_progress_view.dart';
+import 'package:frankencoin_wallet/src/widgets/wallet_connect/bottom_sheet_message_display.dart';
 
 class SwapProgressModal extends StatefulWidget {
   final bool isApproved;
@@ -43,16 +46,38 @@ class _SwapProgressModalState extends State<SwapProgressModal> {
   _SwapProgress _status = _SwapProgress.created;
 
   Future<void> _approve() async {
-    setState(() => _status = _SwapProgress.approving);
-    final txId = await widget.approveSwap.call();
-    log("Approve tx: $txId");
-    setState(() => _status = _SwapProgress.approved);
+    try {
+      setState(() => _status = _SwapProgress.approving);
+      final txId = await widget.approveSwap.call();
+      log("Approve tx: $txId");
+      setState(() => _status = _SwapProgress.approved);
+    } catch (e) {
+      setState(() => _status = _SwapProgress.created);
+      Navigator.of(context).pop();
+
+      getIt.get<BottomSheetService>().queueBottomSheet(isModalDismissible: true,
+        widget: BottomSheetMessageDisplayWidget(
+          message: '${S.current.error}: ${e.toString()}',
+        ),
+      );
+    }
   }
 
   Future<void> _complete() async {
-    setState(() => _status = _SwapProgress.executing);
-    final txId = await widget.confirmSwap.call();
-    Navigator.of(context).pop(txId);
+    try {
+      setState(() => _status = _SwapProgress.executing);
+      final txId = await widget.confirmSwap.call();
+      Navigator.of(context).pop(txId);
+    } catch (e) {
+      setState(() => _status = _SwapProgress.created);
+      Navigator.of(context).pop();
+
+      getIt.get<BottomSheetService>().queueBottomSheet(isModalDismissible: true,
+        widget: BottomSheetMessageDisplayWidget(
+          message: '${S.current.error}: ${e.toString()}',
+        ),
+      );
+    }
   }
 
   Future<void> _next() async {
